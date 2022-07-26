8005c03193ed4562e3f32629b417a1aa63769475
Marcos <marcosgabrielliamcosta@gmail.com>
2022-07-26 09:37:00 -0300

    git commit "jokepo"

--- fatorial.py
+++ fatorial.py
@@ -2,7 +2,15 @@ def fatorial(n, show=False):
     '''Calcula o fatorial de um número.
     :para n:O número a ser calculado.
     :para show:(opcional) Mostrar ou não a conta.
-    :r'''
+    :return f: retorna a multicacao para formar a multiiplicacao
+    -> CÓDIGO DA DEF:
+        f = 1
+        for c in range(n, 0, -1):
+            if show:
+                print(f'{c}', end="")
+                print(f' x 'if c > 1 else ' = ', end='')
+            f *= c
+        return f'''
     f = 1
     for c in range(n, 0, -1):
         if show:
